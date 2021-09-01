e9fd4a65aaa730845bb5d36c3e57bf10d393fb56
Clon <clonvhs@gmail.com>
2021-09-01 16:29:50 +0300

    Updates

---
+++ precache-manifest.de24c2478732cbb48bf126eb85f7535c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af4daf36d7d12895c4a20b13c536174",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015e39f831fbba010edd",
+    "url": "./static/js/main.4d5aaac4.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.de24c2478732cbb48bf126eb85f7535c.js"
 );
 
 self.addEventListener('message', (event) => {
